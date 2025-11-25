1212a40873c9ccd373bf76cdfd55b7d9f9a07ce4
Bradley Wogsland <bradley@wogsland.org>
2025-11-25 16:56:51 +0100

    #2 mock layout WIP

--- frontend/app/welcome/welcome.tsx
+++ frontend/app/welcome/welcome.tsx
@@ -1,6 +1,3 @@
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
-
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
@@ -8,13 +5,13 @@ export function Welcome() {
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
             <img
-              src={logoLight}
-              alt="React Router"
+              src="../../assets/atm_sign.png"
+              alt="ATM"
               className="block w-full dark:hidden"
             />
             <img
-              src={logoDark}
-              alt="React Router"
+              src="../../assets/atm_sign.png"
+              alt="ATM"
               className="hidden w-full dark:block"
             />
           </div>
@@ -22,7 +19,7 @@ export function Welcome() {
         <div className="max-w-[300px] w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next?
+              Welcome to the ATM
             </p>
             <ul>
               {resources.map(({ href, text, icon }) => (
@@ -40,6 +37,20 @@ export function Welcome() {
               ))}
             </ul>
           </nav>
+        </div>
+        <div className="w-[500px] max-w-[100vw] p-4 items-right">
+          <div className="w-[100px]">
+            <img
+              src="../../assets/systems.png"
+              alt="systems"
+              className="block w-full dark:hidden"
+            />
+            <img
+              src="../../assets/systems.png"
+              alt="systems"
+              className="hidden w-full dark:block"
+            />
+          </div>
         </div>
       </div>
     </main>
