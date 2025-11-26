98568037bd29d28a888ba5728b599116f513dc87
Bradley Wogsland <bradley@wogsland.org>
2025-11-26 17:16:31 +0100

    #21 rough layout (#20)
    * #2 mock layout WIP
    
    * still very rough
    
    * prettier
    
    * lint

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -4,8 +4,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ATM" },
+    { name: "description", content: "Welcome to the ATM" },
   ];
 }
 
