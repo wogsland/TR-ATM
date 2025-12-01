df4ffe9abe38720f0b31e73def79b6477427e831
Bradley Wogsland <bradley@wogsland.org>
2025-12-01 17:46:13 +0100

    #12 depositing (#24)
    * #12 depositing
    
    * double derp

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,6 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("balance", "routes/balance.tsx"),
+  route("deposit", "routes/deposit.tsx"),
   route("pin", "routes/pin.tsx"),
   route("main", "routes/mainmenu.tsx"),
 ] satisfies RouteConfig;
